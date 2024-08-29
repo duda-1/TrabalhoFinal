7000849b88689d067a44d4222d124938a35841b6
Alunos <eduardacristinasilva2008@gmail.com>
2024-08-29 13:58:57 -0300

    v

--- TrabalhoFinal/2-Repository/Data/InicializadorBD.cs
+++ TrabalhoFinal/2-Repository/Data/InicializadorBD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace TrabalhoFinal._2_Repository.Data
 {
     public static class InicializadorBD
     {
+        private const string ConnectionString = "Data Source=CRUD.db";
+
+        public static void Inicializar()
+        {
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string commandoSQL = @"
+                    CREATE TABLE IF NOT EXISTS Times(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    NomeLivro TEXT NOT NULL,
+                    NumPaginas  INTEGER NOT NULL,
+                    EditoraLivro TEXT NOT NULL,
+                    Tipo TEXT NOT NULL,
+                    Classificacoes TEXT NOT NULL,
+                    );";
+
+                using (var command = new SQLiteCommand(commandoSQL, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
