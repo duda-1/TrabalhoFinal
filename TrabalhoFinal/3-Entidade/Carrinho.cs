cfba8d87d42a536d35f71837d613b2157d69db7d
Alunos <eduardacristinasilva2008@gmail.com>
2024-09-06 17:20:37 -0300

    c

---
+++ TrabalhoFinal/3-Entidade/Carrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinal._3_Entidade
+{
+    public class Carrinho
+    {
+        int Id { get; set; }
+        public int ClienteId { get; set; }  
+        public int CarrinhoId { get; set; }
+    }
+}
