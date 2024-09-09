ec612db504840282d0369c410fd2dbfe7960e28a
Alunos <eduardacristinasilva2008@gmail.com>
2024-09-09 17:22:34 -0300

    v

---
+++ TrabalhoFinal/3-Entidade/DTO/CreateLivroDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoFinal._1_Service.DTO
+{
+    public class CreateLivroDTO
+    {
+        public string NomeLivro { get; set; }
+        public int NumPaginas { get; set; }
+        public string EditoraLivro { get; set; }
+        public string NomeAutor { get; set; }
+    }
+}
