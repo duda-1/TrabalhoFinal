87a2dc9b3a4c3927688406be6291697a8db86b95
Alunos <eduardacristinasilva2008@gmail.com>
2024-09-18 17:15:55 -0300

    c

--- TrabalhoFinal/2-Repository/CarrinhoRepository.cs
+++ TrabalhoFinal/2-Repository/CarrinhoRepository.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrabalhoFinal._3_Entidade;
+using TrabalhoFinal._3_Entidade.DTO;
 
 namespace TrabalhoFinal._2_Repository
 {
@@ -32,9 +33,25 @@ namespace TrabalhoFinal._2_Repository
 
         public List<Carrinho> Listar()
         {
-            //mexer no listar
+           // mexer no listar
             using var connection = new SQLiteConnection(ConnectionString);
             return connection.GetAll<Carrinho>().ToList();
+
+            //using var connection = new SQLiteConnection(ConnectionString);
+            //List<Carrinho> carrinho = connection.GetAll<Carrinho>().ToList();
+            //List<CreateCarrinhoDTO> carrinhoDTO = new List<CreateCarrinhoDTO>();//_mapper.Map<List<ReadRotinaDTO>>(lista);
+            //foreach (Carrinho c in carrinho)
+            //{
+            //    CreateCarrinhoDTO carrinhoDTO = new CreateCarrinhoDTO();
+            //    carrinhoDTO.ClienteId= c.ClienteId;
+            //    carrinhoDTO.LivroId= c.LivroId;
+            //    carrinhoDTO.Dia = _repositoryDia.BuscarPorID(r.DiaId);
+            //    carrinhoDTO.PessoaId = r.PessoaId;
+            //    carrinhoDTO.Pessoa = _repositoryPessoa.BuscarPorId(r.PessoaId);
+            //    carrinhoDTO.Atividade = _repositoryAtividade.BuscarPorId(r.AtividadeId);
+            //    carrinhoDTO.Add(carrinhoDTO);
+            //}
+            //return carrinhoDTO;
         }
 
         public void Editar(Carrinho c)
